8bbe55ed974772bc8997585fe61c868c625e9358
MEER WAFEEQ ALI <meerwafeeqali@gmail.com>
2026-08-04 15:19:26 +0530

    Refactor scroll event listener for navbar

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
 const navbar = document.getElementById("navbar");
 
-window.addEventListener("scroll", function () {
+window.addEventListener("scroll", function(){
 
-    if (window.scrollY > 50) {
+    if(window.scrollY > 50){
+
         navbar.classList.add("scrolled");
-    } else {
+
+    }
+
+    else{
+
         navbar.classList.remove("scrolled");
+
     }
 
 });
